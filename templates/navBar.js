12ead9b2d5d5812256e5675396883f6022024c75
Jackson Guerriero <jackson@guerriero.info>
2023-01-14 21:04:20 -0500

    line break updates

--- templates/navBar.js
+++ templates/navBar.js
@@ -14,13 +14,19 @@ template.innerHTML = `
             </innerFrameTextH>
             <br>
             <br>
+            <br>
+            <br>
             <innerFrameText>
                 <a href="home.html">Home</a>
                 <br>
                 <br>
                 <br>
                 <br>
+                <br>
+                <br>
                 <a href="aboutme.html">About Me</a>
+                <br>
+                <br>
                 <br>
                 <br>
                 <br>
